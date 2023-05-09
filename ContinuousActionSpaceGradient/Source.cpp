c7d8fd12dc5f29bb1c9bfbe7e4c30862f7bf1cda
theProgrammingBox <justinboxgea@gmail.com>
2023-05-09 13:12:34 -0700

    this kind of works

--- ContinuousActionSpaceGradient/Source.cpp
+++ ContinuousActionSpaceGradient/Source.cpp
@@ -1,38 +1,52 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 #include <random>
+#include <algorithm>
 
 int main()
 {
+	const uint32_t samples = 10;
+	float actions[samples];
+
 	olc::vf2d policy = { 0.0f, 1.0f };
-	olc::vf2d targetPolicy = { -10.0f, 0.4f };
+	olc::vf2d targetPolicy = { -100.0f, 100.0f };
 	olc::vf2d policyGrad;
 
 	std::random_device rd;
 	std::mt19937 gen(rd());
-	std::uniform_real_distribution<> dis(0.0f, 1.0f);
+	std::normal_distribution<> dis(0.0f, 1.0f);
 
 	while (true)
 	{
-		float action = policy.x + policy.y * dis(gen);
-		float action2 = policy.x + policy.y * dis(gen);
 		float targetAction = targetPolicy.x + targetPolicy.y * dis(gen);
 
-		float error = targetAction - action;
-		float error2 = targetAction - action2;
+		for (uint32_t i = 0; i < samples; i++)
+			actions[i] = policy.x + policy.y * dis(gen);
 
-		if (error2 < error)
+		// sort actions by errors closest to 0
+		std::sort(actions, actions + samples, [targetAction](float a, float b) { return std::abs(targetAction - a) < std::abs(targetAction - b); });
+
+		for (uint32_t i = samples * 0.5f; i < samples; i++)
 		{
-			action = action2;
-			error = error2;
+			float tmp = 1.0f / policy.y;
+			policyGrad.x = (actions[i] - policy.x) * tmp * tmp;
+			policyGrad.y = policyGrad.x * policyGrad.x * policy.y - tmp;
+
+			policy -= policyGrad * 0.01f;
+			policy.y = std::max(0.2f, policy.y);
 		}
-		float tmp = 1.0f / policy.y;
-		policyGrad.x = (action - policy.x) * tmp * tmp;
-		policyGrad.y = policyGrad.x * policyGrad.x * policy.y - tmp;
 
-		policy += 0.1f * error * policyGrad;
+		for (uint32_t i = samples * 0.5f; i--;)
+		{
+			float tmp = 1.0f / policy.y;
+			policyGrad.x = (actions[i] - policy.x) * tmp * tmp;
+			policyGrad.y = policyGrad.x * policyGrad.x * policy.y - tmp;
 
-		printf("error: %f, policy: %f, %f\n", error, policy.x, policy.y);
+			policy += policyGrad * 0.01f;
+			policy.y = std::max(0.2f, policy.y);
+		}
+
+		printf("Policy: %f %f\n", policy.x, policy.y);
 	}
 
 	return 0;
